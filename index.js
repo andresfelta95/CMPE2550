fb81ec0ea03eab0ab5253afd1aeb0cf2db77921e
andresfelta95 <andresfelta95@gmail.com>
2022-09-27 07:13:40 -0600

    Improvement on the landing page, need to fix bugs with the Nav bar

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 // Language: javascript
 // Path: CMPE2550\index.js
 // Change nav bar depending on screen size
-$(document).ready(function() {
-    $('#menu-toggle').on('click', function() {
-        $('.navbar').toggleClass('expand');
-    });
-});
+(function($) { 
+    $(function() { 
+  
+      //  open and close nav 
+      $('#navbar-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+  
+  
+      // Hamburger toggle
+      $('#navbar-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+  
+  
+      // If a link has a dropdown, add sub menu toggle.
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.navbar-dropdown').slideToggle("slow");
+  
+        // Close dropdown when select another dropdown
+        $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+        e.stopPropagation();
+      });
+  
+  
+      // Click outside the dropdown will remove the dropdown class
+      $('html').click(function() {
+        $('.navbar-dropdown').hide();
+      });
+    }); 
+  })(jQuery); 
